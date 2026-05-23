3ef4c1558e3647f446966a492581e6c62ec80333
dung382 <dinhvietdung3003thd@gmail.com>
2026-05-23 23:31:01 +0700

    add CORS for USER page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->alias([
             'jwt.auth' => \App\Http\Middleware\JwtAuthenticate::class,
             'custom.jwt' => \App\Http\Middleware\JwtAuthenticate::class,
